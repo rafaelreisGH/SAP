17f9345ac98b07fdc82a59267cee375d51364442
rafaelreisGH <45438038+rafaelreisGH@users.noreply.github.com>
2022-03-03 17:36:25 -0400

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8645c3248348aa930d3cd9159ba02131d2627a87',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8645c3248348aa930d3cd9159ba02131d2627a87',
+            'dev_requirement' => false,
+        ),
+    ),
+);
